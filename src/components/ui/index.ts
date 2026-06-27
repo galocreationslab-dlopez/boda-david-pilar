8373c9137a46e54df1b11ffb48ee62d2d1b21340
galocreationslab-dlopez <galocreationslab@gmail.com>
2026-06-27 10:30:09 +0000

    Añado archivos que faltan

---
+++ src/components/ui/index.ts
@@ -0,0 +1,8 @@
+/**
+ * components/ui/index.ts
+ * Exportaciones centralizadas de componentes UI reutilizables
+ */
+
+export { SelloNupcial } from "./SelloNupcial";
+export { CuentaAtras } from "./CuentaAtras";
+export { OrnamentoDivisor, SeparadorSeccion } from "./OrnamentoDivisor";
